93c28a9fc4dba547f105c8156aa65a391abb9091
Germinal Camps <gcampsbarjau@gmail.com>
2020-02-17 12:18:22 +0100

    looping through data

--- src/main.js
+++ src/main.js
@@ -2,12 +2,18 @@ import Vue from "vue";
 import App from "./App.vue";
 import VueRouter from "vue-router";
 import router from "./router";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
 
 Vue.use(VueRouter);
+Vue.use(Vuetify);
+
+const vuetify = new Vuetify({});
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
